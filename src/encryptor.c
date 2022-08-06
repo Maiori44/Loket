3751f647f88889642368f0cd71cb5d1d317fd2db
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-08-07 00:09:04 +0200

    Add #ifndef

--- src/encryptor.c
+++ src/encryptor.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include "encryptor.h"
+#include "dynstack.h"
 
 void encryptfile(char *path, dynstack *chars, size_t pathlen) {
 
--- src/encryptor.h
+++ src/encryptor.h
@@ -1,5 +1,10 @@
+#ifndef ENCRYPTOR
+#define ENCRYPTOR
+
 #include <stdlib.h>
 #include "dynstack.h"
 
 void encryptfile(char *path, dynstack *chars, size_t pathlen);
 void decryptfile(char *path, dynstack *chars, size_t pathlen);
+
+#endif
