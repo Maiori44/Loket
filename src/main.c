5f7d35d78f60c484776cd073e86e7eb58ea1b1f9
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-08-06 12:53:21 +0200

    Add basic code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
-    puts("Hello world!");
+int gargc;
+char **gargv;
+char *getarg(int n, char *msg);
+
+int main(int argc, char *argv[]) {
+	gargc = argc;
+	gargv = argv;
+	char *mode = getarg(1, "Insert mode: (encrypt/decrypt)");
+	free(mode);
+	return 0;
+}
+
+char *getarg(int n, char *msg) {
+	if (n < gargc)
+		return gargv[n];
+	else {
+		puts(msg);
+		char *result = malloc(sizeof(char) * 200);
+		scanf(" %[^\n]%*c", result);
+		size_t len;
+		for (len = strlen(result); isspace(result[len - 1]); --len)
+			result[len - 1] = '\0';
+		if (len >= 200) {
+			puts("The given text is too long!");
+			exit(1);
+		}
+		return result;
+	}
 }
