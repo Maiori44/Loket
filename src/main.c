5000eb1d0696e80018ea91394c3c889bb31e8f67
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-08-06 15:41:59 +0200

    Stuff

--- src/main.c
+++ src/main.c
@@ -11,24 +11,29 @@ int main(int argc, char *argv[]) {
 	gargc = argc;
 	gargv = argv;
 	char *mode = getarg(1, "Insert mode: (encrypt/decrypt)");
+	char *path = getarg(2, "Insert path:");
+	char *key  = getarg(3, "Insert key:");
+	//printf("Mode: %s\nPath: %s\nKey:  %s\n", mode, path, key);
 	free(mode);
+	free(path);
+	free(key);
 	return 0;
 }
 
 char *getarg(int n, char *msg) {
+	char *result = malloc(sizeof(char) * 200);
 	if (n < gargc)
-		return gargv[n];
+		result = gargv[n];
 	else {
 		puts(msg);
-		char *result = malloc(sizeof(char) * 200);
 		scanf(" %[^\n]%*c", result);
-		size_t len;
-		for (len = strlen(result); isspace(result[len - 1]); --len)
-			result[len - 1] = '\0';
-		if (len >= 200) {
-			puts("The given text is too long!");
-			exit(1);
-		}
-		return result;
 	}
+	size_t len;
+	for (len = strlen(result); isspace(result[len - 1]); --len)
+		result[len - 1] = '\0';
+	if (len >= 200) {
+		puts("The given text is too long!");
+		exit(1);
+	}
+	return result;
 }
