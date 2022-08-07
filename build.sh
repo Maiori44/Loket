db855bba8281b09b6098f9924e3bbb6a263152f3
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-08-07 19:27:36 +0200

    Remove debug echos

--- build.sh
+++ build.sh
@@ -2,9 +2,7 @@
 clear
 cd src
 if [[ "$1" == "-win" ]]; then
-	echo Win
 	x86_64-w64-mingw32-gcc-win32 main.c dynstack.c -o main.exe -Wall && wine ./main.exe $2 $3
 else
-	echo Linux
 	gcc main.c dynstack.c -o main -Wall && ./main $1 $2
 fi
