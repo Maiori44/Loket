5c243db5fa47c45571d3eeb590631b0150a48f5b
Felice D'Angelo <felice.dangelo2013@gmail.com>
2022-08-07 19:17:17 +0200

    Cross compile for windows on linux

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 clear
 cd src
-gcc main.c dynstack.c -o main -Wall && ./main $1 $2
+if [[ "$1" == "-win" ]]; then
+	echo Win
+	x86_64-w64-mingw32-gcc-win32 main.c dynstack.c -o main.exe -Wall && wine ./main.exe $2 $3
+else
+	echo Linux
+	gcc main.c dynstack.c -o main -Wall && ./main $1 $2
+fi
